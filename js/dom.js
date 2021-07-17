860cb9ceb8fc198d2c4a1e056e02fd9941a38fd8
kucengorenji <mikelrisyad@gmail.com>
2021-07-18 05:22:23 +0700

    add some code

---
+++ js/dom.js
@@ -0,0 +1,48 @@
+const UNCOMPLETED_BOOK_ID = "book";
+const COMPLETED_BOOK_ID = "completed-book";
+const BOOK_ITEMID = "bookId";
+
+/* 
+
+    id: 3657848524,
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J.K Rowling",
+    year: 1997,
+    isComplete: false,
+
+*/
+
+/*
+
+        
+    <!--            <article class="book_item">-->
+    <!--                <h3>Book Title</h3>-->
+    <!--                <p>Penulis: John Doe</p>-->
+    <!--                <p>Tahun: 2002</p>--
+    <!--                <div class="action">-->
+    <!--                    <button class="green">Selesai dibaca</button>-->
+    <!--                    <button class="red">Hapus buku</button>-->
+    <!--                </div>-->
+    <!--            </article>-->
+        
+
+*/
+
+function inputBook(title, author, year) {
+    const bookTitle = document.createElement("h3");
+    bookTitle.innerText = title;
+
+    const bookAuthor = document.createElement("p")
+    bookAuthor.innerText = `Penulis: ${author}`;
+
+    const bookYear = document.createElement("p");
+    bookYear.innerText = `Tahun: ${year}`
+
+    const textContainer = document.createElement("article");
+    textContainer.classList.add("book_item")
+    textContainer.append(bookTitle, bookAuthor, bookYear);
+
+    return textContainer;
+
+
+}
